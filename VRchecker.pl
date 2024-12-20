dd69640e80fc6f862c59dd3e0c1fcff0f85fe2de
srhartono <srhartono@ucdavis.edu>
2024-12-19 18:34:07 -0800

    Initial Commit

--- VRchecker.pl
+++ VRchecker.pl
@@ -32,18 +32,19 @@ our $LPR    ="\e[1;35m";
 
 my ($fqFile, $VRFile) = ($opt_i, $opt_b);
 my $usage = "
-Usage: $YW$0$N -i $LCY<fqFile>$N -b $LPR<VRFile>$N
+Usage: $YW$0$N -i $LCY<fqFile.fastq.gz>$N -b $LPR<VRFile.fa>$N
 
 -o: output dir [currrent directory]
--l: min match length threshold [100]
+-l: min continuous match length threshold [200]
+-q: quiet, don't print anything
 
 Examples:
 
-- Exact match only (assuming VR sequence length is 200bp)
-$YW$0$N -i ${LCY}001_pFC8-VR-TAC-2x-LAC-Mini-1_reads.fastq.gz$N -b ${LGN}VR.fa$N -l 200
+- Default: Exact match only (assuming VR sequence length is 200bp)
+$YW$0$N -i ${LCY}example.fastq.gz$N -b ${LGN}example_VR.fa$N -l ${LPR}200$N
 
 - At least 100bp of VR sequence match:
-$YW$0$N -i ${LCY}001_pFC8-VR-TAC-2x-LAC-Mini-1_reads.fastq.gz$N -b ${LGN}VR.fa$N -l 100
+$YW$0$N -i ${LCY}example.fastq.gz$N -b ${LGN}example_VR.fa$N -l ${LPR}100$N
 
 More options & explanation do -H
 
@@ -58,7 +59,7 @@ Options for quick dirty sliding-window based non-exact matching (blast takes too
 
 If the whole VR length doesn't match (200bp) it'll try to reduce the window size by [-w] 
 and do sliding window matching with step size of [-s]
-It'll stop when finding a match or when window size is less than min length threshold [-l, default 100]
+It'll stop when finding a match or when window size is less than min length threshold [-l, default 200]
 
 Example: -s step size 20, -w window 25, min match length threshold$LGN 100$N
 
@@ -69,16 +70,17 @@ window = 150 - 25 = 125 bp -> substring position 0-125, 20-145, 40 - 165, 60 - 1
 window = 125 - 25 = 100 bp -> substring position 0-100, 20-120, 40-140, 60-160, 80-180, 100-200
 window = 100 - 25 =  75 bp -> search stop here coz window length (75bp) is less than min match length threshold -l 100bp
 
+If min match length threshold is same as VR size (e.g. 200bp), then step size/window doesn't matter
 ";
 
 die $usage unless defined $opt_i and defined $opt_b;
 die $usage . "\n" . $usage_long if defined $opt_H;
 die "fastq file -i $LCY$fqFile$N doesn't exist!\n" if not -e $fqFile;
 die "query file -b $LCY$VRFile$N doesn't exist!\n" if not -e $VRFile;
-my $step_size            = defined $opt_s ? $opt_s : 25;
-my $window_subtract_size = defined $opt_w ? $opt_w : 25;
-my $minlen_threshold     = defined $opt_l ? $opt_l : 100;
-my $outDir               = defined $opt_o ? $opt_o : "./";
+my $step_size             = defined $opt_s ? $opt_s : 20;
+my $window_subtract_size  = defined $opt_w ? $opt_w : 25;
+my $minlen_threshold_orig = defined $opt_l ? $opt_l : 0;
+my $outDir                = defined $opt_o ? $opt_o : "./";
 my $quiet = 1 if defined $opt_q;
 #
 #" unless @ARGV;
@@ -87,10 +89,10 @@ system("mkdir -p $outDir") if not -d $outDir;
 $outDir =~ s/\/+$//;
 my ($folder1, $fileName1) = getFilename($fqFile, "folderfull");
 my ($folder2, $VRFilename) = getFilename($VRFile, "folderfull");
-my $logFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold\_w$window_subtract_size\_s$step_size.log";
-my $outFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold\_w$window_subtract_size\_s$step_size.tsv";
-my $outFileshort = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold\_w$window_subtract_size\_s$step_size.short.tsv";
-my $VRstatsFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold\_w$window_subtract_size\_s$step_size.stats";
+my $logFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold_orig\_w$window_subtract_size\_s$step_size.log";
+my $outFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold_orig\_w$window_subtract_size\_s$step_size.tsv";
+my $outFileshort = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold_orig\_w$window_subtract_size\_s$step_size.short.tsv";
+my $VRstatsFile = "$outDir/$fileName1\_$VRFilename\_l$minlen_threshold_orig\_w$window_subtract_size\_s$step_size.stats";
 
 open (my $outLog, ">", $logFile) or die "Can't write to $logFile: $!\n";
 my %out;
@@ -100,8 +102,8 @@ Parameters:$N
 -b VR/query fasta file   : $LCY$VRFile$N
 -o output dir            : $LCY$outDir$N
 -s step size             : $LGN$step_size$N
--w window subtract size : $LGN$window_subtract_size$N
--l min length threshold  : $LGN$minlen_threshold$N
+-w window subtract size  : $LGN$window_subtract_size$N
+-l min length threshold  : $LGN$minlen_threshold_orig$N
 
 ",$opt_q);
 LOG($outLog, "\n#queries:\n",$opt_q);
@@ -117,13 +119,11 @@ while (my $line = <$in0>) {
 	else {
 		($VRname, $VR) = split("\t", $line);
 	}
-	my $VR2 = $VR;
-	$VR2 =~ s/^(.)/N/;
 	$VR{$VR} = $VRname;
-	#$VR{$VR2} = $VRname;
+	my $VRlen = length($VR);
 	my $VRshort = $VR;
 	($VRshort) = $VR =~ /^(.{20})/ if length($VR) >= 20;
-	print "$LCY$VRname$N\t$YW$VRshort$N...\n" if not defined $opt_q;
+	print "$LCY$VRname$N\t$YW$VRshort$N... ($LGN$VRlen$N bp)\n" if not defined $opt_q;
 	LOG($outLog, "$VRname\t$VR\n",1);
 }
 close $in0;
@@ -171,60 +171,63 @@ while (my $line = <$inFastQ>) {
 	my @bcseq;
 	#print "foreach\n" if ($read_name eq "\@6acb2984-5480-415b-96db-da1208fa1a0d");
 	foreach my $VRorig (sort keys %VR) {
-		my $minlen = 200;
+		my $minlen = length($VRorig);
+		my $init = 0;
 		my $found = 0;
 		my $VRrevorig = revcomp($VRorig);
+		my $minlen_threshold = $minlen_threshold_orig eq 0 ? length($VRorig) : $minlen_threshold_orig;
 		#print "   while $LGN$VR{$VRorig}$N\n" if ($read_name eq "\@6acb2984-5480-415b-96db-da1208fa1a0d");
-		while ($minlen >= $minlen_threshold) {
-		my $i = -1;
-		while (1) {
-			my ($VR, $VRrev);
-			if ($i == -1) {
-				($VR, $VRrev) = ($VRorig, $VRrevorig);
-			}
-			else {
-				my $substr = $minlen > length($VRorig) ? length($VRorig) : $minlen;
-				$VR = substr($VRorig, $i, $substr);
-				$VRrev = revcomp($VR);
-			}
-			if ($read_seq =~ /($VR|$VRrev)/) {
-				if ($read_seq =~ /$VR/) {
-					($VRseqbeg, $VRseqend) = $read_seq =~ /^(.*)$VR(.*)$/;
-					$VRfinal = $VR{$VRorig};
-					$VRseqfinal = $VR;
-					$VRstrand = "+";
+		while ($init == 0 or $minlen >= $minlen_threshold) {
+			$init = 1;
+			my $i = -1;
+			while (1) {
+				my ($VR, $VRrev);
+				if ($i == -1) {
+					($VR, $VRrev) = ($VRorig, $VRrevorig);
 				}
-				elsif ($read_seq =~ /$VRrev/) {
-					($VRseqbeg, $VRseqend) = $read_seq =~ /^(.*)$VRrev(.*)$/;
-					$VRfinal = $VR{$VRorig};
-					$VRseqfinal = $VRrev;
-					$VRstrand = "-";
+				else {
+					my $substr = $minlen > length($VRorig) ? length($VRorig) : $minlen;
+					$VR = substr($VRorig, $i, $substr);
+					$VRrev = revcomp($VR);
 				}
-				$totalbc ++;
-				$beg = length($VRseqbeg);
-				$end = length($read_seq) - length($VRseqend);
-				my $VRseqbegdot = join("", ("-") x length($VRseqbeg));
-				my $VRseqenddot = join("", ("-") x length($VRseqend));
-				#if ($totalbc == 1) {
-				push(@bc, $VRfinal);
-				push(@bcseq, $VRseqfinal);
-				#	$data{sample}{$VRfinal} ++;
-				#	$data{samplebc}{$VRseqfinal} ++;
-				#}
-				if ($totalbc == 2) {
-					$data{multiplebc} ++;
+				if ($read_seq =~ /($VR|$VRrev)/) {
+					if ($read_seq =~ /$VR/) {
+						($VRseqbeg, $VRseqend) = $read_seq =~ /^(.*)$VR(.*)$/;
+						$VRfinal = $VR{$VRorig};
+						$VRseqfinal = $VR;
+						$VRstrand = "+";
+					}
+					elsif ($read_seq =~ /$VRrev/) {
+						($VRseqbeg, $VRseqend) = $read_seq =~ /^(.*)$VRrev(.*)$/;
+						$VRfinal = $VR{$VRorig};
+						$VRseqfinal = $VRrev;
+						$VRstrand = "-";
+					}
+					$totalbc ++;
+					$beg = length($VRseqbeg);
+					$end = length($read_seq) - length($VRseqend);
+					my $VRseqbegdot = join("", ("-") x length($VRseqbeg));
+					my $VRseqenddot = join("", ("-") x length($VRseqend));
+					#if ($totalbc == 1) {
+					push(@bc, $VRfinal);
+					push(@bcseq, $VRseqfinal);
+					#	$data{sample}{$VRfinal} ++;
+					#	$data{samplebc}{$VRseqfinal} ++;
+					#}
+					if ($totalbc == 2) {
+						$data{multiplebc} ++;
+					}
+					#my $length = $end - $beg;
+					my $length = length($VRseqfinal);
+					print $out0 "$read_name\t$beg\t$end\t$VRfinal\t$totalbc;$i;$length\t$VRstrand\t$VRseqbegdot$VRseqfinal$VRseqenddot\t$read_seq\t$read_qual\n";
+					#print "$YW$read_name$N: $LCY$VRfinal$N ($beg-$end $VRstrand)\n$LGN$read_seq$N\n$LPR$VRseqbegdot$LGN$VRseqfinal$N$LPR$VRseqenddot$N\n\n";
+					#print "   i=$LGN$i$N: LCY$VRfinal$N $LGN$VR{$VRorig}$N\n" if ($read_name eq "\@6acb2984-5480-415b-96db-da1208fa1a0d");
+					$found = 1;
+					last if $found eq 1;
 				}
-				#my $length = $end - $beg;
-				my $length = length($VRseqfinal);
-				print $out0 "$read_name\t$beg\t$end\t$VRfinal\t$totalbc;$i;$length\t$VRstrand\t$VRseqbegdot$VRseqfinal$VRseqenddot\t$read_seq\t$read_qual\n";
-				#print "$YW$read_name$N: $LCY$VRfinal$N ($beg-$end $VRstrand)\n$LGN$read_seq$N\n$LPR$VRseqbegdot$LGN$VRseqfinal$N$LPR$VRseqenddot$N\n\n";
-				#print "   i=$LGN$i$N: LCY$VRfinal$N $LGN$VR{$VRorig}$N\n" if ($read_name eq "\@6acb2984-5480-415b-96db-da1208fa1a0d");
-				$found = 1;
+				$i += $step_size;
+				last if length($VRorig) < $i + $minlen;
 				last if $found eq 1;
-			}
-			$i += $step_size;
-			last if length($VRorig) < $i + $minlen;
-			last if $found eq 1;
 			}
 			$minlen -= $window_subtract_size;
 			last if length($VRorig) < $i + $minlen;
